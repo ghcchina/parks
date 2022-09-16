cd351c8642949207d31068f859784e43eaa375b5
Hongchao Guan <ghcchina@gmail.com>
2022-09-15 20:38:57 -0700

    Initial Commit

---
+++ SaySomething/SaySomethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaySomethingApp.swift
+//  SaySomething
+//
+//  Created by  Hongchao Guan on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SaySomethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
